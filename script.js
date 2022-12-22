f71a799547980df21c0143f6219b2104d96a6933
VincentDoingCode <113548490+VincentDoingCode@users.noreply.github.com>
2022-12-22 13:08:56 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var bugsAlive = 15;
 var bugsDead = 0;
-
+var empty = false;
+var commentArr = [];
 function initialize(){
     
+}
+
+function clickToSquash(){
+   
+}
+
+function isSquashed(){
+
 }
