39f9ce841afa226d8d2531bed5a779b1806be633
VincentDoingCode <113548490+VincentDoingCode@users.noreply.github.com>
2022-12-23 14:03:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ function initialize(){
     
 }
 
-function clickToSquash(){
-   
+function clickToSquash(id){
+   document.getElementById(String(id)).src= "images/splat.png";
+   bugsAlive--;
 }
 
-function isSquashed(){
 
+// function clickToSquash(){
+//     if(document.getElementById("bug1").src == "BugSplat-main\images\bug.png"){
+//         document.getElementById("bug1").src = "images/splat.png";
+//     }
+//     else{
+//         document.getElementById("bug1").src = "images/splat.png";
+//         console.log("DARN");
+//     }
+//  }
+function comments(){
+    if(bugsAlive > 8){
+        
+    }
 }
