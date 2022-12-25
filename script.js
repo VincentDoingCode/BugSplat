37b1186346d86736295385a4cbe3acde3b17ae6d
VincentDoingCode <113548490+VincentDoingCode@users.noreply.github.com>
2022-12-25 15:08:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var bugsAlive = 15;
 var bugsDead = 0;
 var empty = false;
-var commentArr = [];
+var aggressiveCom = ["Who do you think you are?", "Splat us again I dare you", "You suck", "We will destroy you", "DIE", "How dare you","Our mother will squash you","Lowly human","Back off you mammal","This is your last chance human", "Our brothers and sisters will destroy you", "(Bug Noises)", "Move","Go do something fun, loser","LEAVE"];
+var pleadingCom = ["Please", "Don't squash us anymore","I beg off you stop","Just take the money and leave please", "What do you want", "Forgive us","Take anything you want", "*CRYS*", "I'll do anything for you to stop"];
+
 function initialize(){
     
 }
 
-function clickToSquash(id){
-   document.getElementById(String(id)).src= "images/splat.png";
-   bugsAlive--;
+function clickToSquash(id, com){
+    var temp = document.getElementById(String(id));
+    if(temp.getAttribute('src') === 'images/bug.png'){
+        temp.src = "images/splat.png";
+        temp.style.width = "200px";
+        bugsAlive--;
+        comments();
+    }
+    if(bugsAlive == 0){
+        alert("You've splat us all");
+    }
 }
 
 
@@ -22,7 +32,22 @@ function clickToSquash(id){
 //     }
 //  }
 function comments(){
+    var randomAggNum = getRandomInteger(0, aggressiveCom.length-1);
+    var randomPleadNum = getRandomInteger(0, pleadingCom.length-1);
+    console.log(randomAggNum);
+    console.log(aggressiveCom[randomAggNum]);
     if(bugsAlive > 8){
-        
+        document.getElementById("textComment").value += aggressiveCom[randomAggNum] + '\r\n';
+        aggressiveCom.splice(randomAggNum,randomAggNum);
     }
+    else{
+        console.log(pleadingCom[randomPleadNum]);
+        document.getElementById("textComment").value += pleadingCom[randomPleadNum] + '\r\n';
+    }
+}   
+
+function getRandomInteger(lower, upper){
+    var multiplier = upper - (lower - 1);
+    var rnd = parseInt(Math.random()*multiplier) + lower;
+    return rnd;
 }
